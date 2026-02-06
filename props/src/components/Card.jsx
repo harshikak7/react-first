0ede316e48e8c07427a88180d65268ad2570c980
harshikak7 <harshikakolekar184@gmail.com>
2026-02-06 21:34:16 +0530

    components done

---
+++ props/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Harshika Kolekar</h1>
+        <p>Hi, I’m Harshika Kolekar, a UI/UX Designer who loves turning ideas into clean, human-centered designs that solve real problems.</p>
+        <button>Click Me</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
